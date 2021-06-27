cc86b14b97ee9551bac635d56910cba4396a65c8
sahilsingh7735 <sahilsingh7735@gmail.com>
2021-06-27 14:52:06 +0530

    updated Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,11 +6,12 @@ const Login = (props) => {
                 <CTA>
                     <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
                     <SignUp>GET ALL THERE</SignUp>
-
+                    <Description>This Disney is made by Sahil Singh</Description>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
                 </CTA>
                 <BgImage />
             </Content>
-        </Container>
+        </Container >
     );
 };
 
@@ -49,37 +50,51 @@ const BgImage = styled.div`
             `;
 
 const CTA = styled.div`
-max-width: 650px;
-width: 100%;
-display: flex;
-flex-direction: column;
-`;
+            max-width: 650px;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            `;
 
 const CTALogoOne = styled.img`
-    margin-bottom: 12px;
-    max-width:600px;
-    min-height:1px;
-    display:block;
-    width: 100%;
-`;
+            margin-bottom: 12px;
+            max-width:600px;
+            min-height:1px;
+            display:block;
+            width: 100%;
+            `;
 
 const SignUp = styled.a`
-font-weight: bold;
-color: #f9f9f9;
-background-color: #0063e5;
-margin-bottom: 12px;
-width: 100%;
-letter-spacing: 1.5px;
-font-size: 18px;
-padding: 16.5px 0;
-border: 1px solid transparent;
-border-radius: 100px;
+            font-weight: bold;
+            color: #f9f9f9;
+            background-color: #0063e5;
+            margin-bottom: 12px;
+            width: 100%;
+            letter-spacing: 1.5px;
+            font-size: 18px;
+            padding: 16.5px 0;
+            border: 1px solid transparent;
+            border-radius: 100px;
+            &:hover {
+              background-color: #0483ee;
+            }
+          `;
 
-&:hover {
-    background-color: #0483ee;
-  }
 
+const Description = styled.p`
+            color: hsla(0 ,0%,95.3%,1);
+            font-size: 11px;
+            margin: 0 0 24px;
+            line-height: 1.5;
+            letter-spacing: 1.5px;
+            `;
 
+const CTALogoTwo = styled.img`
+            max-width: 600px;
+            margin-bottom: 20px;
+            display: inline-block;
+            vertical-align: bottom;
+            width: 100%;
+          `;
 
-`;
 export default Login;
