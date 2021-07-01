6c35dd310e3cd2bd5742e1a471e136b5d7e62b3f
soumik2012 <66599363+soumik2012@users.noreply.github.com>
2021-07-01 22:16:50 +0530

    Adding more feature to the app

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,72 @@
 import styled from 'styled-components';
 import ImgSlider from "./ImgSlider";
+import Viewers from './Viewers';
+import Recommends from "./Recommends";
+import NewDisney from './NewDisney';
+import Originals from './Originals';
+import Trending from './Trending';
+import { useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+
+import db from '../firebase';
+import { setMovies } from '../features/user/movie/movieSlice';
+import { selectUserName } from '../features/user/userSlice';
 
 const Home = (props) => {
-    return <Container>
+  const dispatch = useDispatch()
+  const useName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    console.log("hello");
+    db.collection("movies").onSnapshot((snapshot) => {
+      snapshot.docs.map((doc) => {
+        console.log(recommends);
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "new":
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "trending":
+            trending = [...trending, { id: doc.id, ...doc.data() }];
+            break;
+        }
+      });
+    
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+  }, [userName]);
+
+
+    return (
+    <Container>
         <ImgSlider />
-    </Container>;
-}
+        <Viewers />
+        <Recommends/>
+        <NewDisney />
+        <Originals />
+        <Trending />
+    </Container>
+    );
+};
 
 const Container = styled.main`
   position: relative;
